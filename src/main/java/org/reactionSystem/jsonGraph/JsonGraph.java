f067ac06bc03900f79cf011b081c48c1598a7f83
Davide Fissore <fisso9900@gmail.com>
2023-03-13 19:34:18 +0100

    Json to Graph #5

--- src/main/java/org/reactionSystem/jsonGraph/JsonGraph.java
+++ src/main/java/org/reactionSystem/jsonGraph/JsonGraph.java
@@ -6,18 +6,55 @@ import org.reactionSystem.Graph;
 import org.reactionSystem.Node;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class JsonGraph {
 
-    public static class NodeData{
+    protected List<JsonNode> nodes;
+    protected List<JsonEdge> edges;
+
+    private JsonGraph() {
+        nodes = new ArrayList<>();
+        edges = new ArrayList<>();
+    }
+
+    public static String generateJSONGraph(Graph g) throws JsonProcessingException {
+        JsonGraph graph = new JsonGraph();
+        NodeData nd;
+        EdgeData ed;
+        JsonNode jn;
+        JsonEdge je;
+        List<String> classes = new ArrayList<>();
+        classes.add(0, "postype");
+        classes.add(null);
+        for (Node n1 : g.getNodes().values()) {
+            nd = new NodeData(n1);
+            jn = new JsonNode(nd, List.of("simpleNode"));
+            graph.nodes.add(jn);
+            for (Node n2 : n1.getSuccessors().values()) {
+                ed = new EdgeData(n1, n2);
+                je = new JsonEdge(ed, classes);
+                graph.edges.add(je);
+            }
+        }
+        return new ObjectMapper().writeValueAsString(graph);
+    }
+
+    public List<JsonNode> getNodes() {
+        return nodes;
+    }
+
+    public List<JsonEdge> getEdges() {
+        return edges;
+    }
+
+    public static class NodeData {
         protected String id;
         protected String name;
 
-        public NodeData(String id, String name) {
-            this.id = id;
-            this.name = name;
+        public NodeData(Node node) {
+            this.id = String.valueOf(node.id);
+            this.name = node.getName().length() == 0 ? "nil" : node.getName();
         }
 
         public String getId() {
@@ -28,7 +65,8 @@ public class JsonGraph {
             return name;
         }
     }
-    public static class JsonNode{
+
+    public static class JsonNode {
         protected NodeData data;
         protected List<String> classes;
 
@@ -46,15 +84,19 @@ public class JsonGraph {
         }
     }
 
-    public static class EdgeData{
+    public static class EdgeData {
         protected String id;
         protected String source;
         protected String target;
+        protected String sourceName;
+        protected String targetName;
 
-        public EdgeData(String id, String source, String target) {
-            this.id = id;
-            this.source = source;
-            this.target = target;
+        public EdgeData(Node source, Node target) {
+            this.id = source.id + "_" + target.id + "_edge";
+            this.source = String.valueOf(source.id);
+            this.target = String.valueOf(target.id);
+            this.sourceName = source.getName();
+            this.targetName = target.getName();
         }
 
         public String getId() {
@@ -68,9 +110,17 @@ public class JsonGraph {
         public String getTarget() {
             return target;
         }
+
+        public String getSourceName() {
+            return sourceName;
+        }
+
+        public String getTargetName() {
+            return targetName;
+        }
     }
 
-    public static class JsonEdge{
+    public static class JsonEdge {
         protected EdgeData data;
         protected List<String> classes;
 
@@ -86,42 +136,5 @@ public class JsonGraph {
         public List<String> getClasses() {
             return classes;
         }
-    }
-    protected List<JsonNode> nodes;
-    protected List<JsonEdge> edges;
-
-    private JsonGraph() {
-        nodes=new ArrayList<>();
-        edges=new ArrayList<>();
-    }
-
-    public List<JsonNode> getNodes() {
-        return nodes;
-    }
-
-    public List<JsonEdge> getEdges() {
-        return edges;
-    }
-
-    public static String generateJSONGraph(Graph g) throws JsonProcessingException {
-        JsonGraph graph=new JsonGraph();
-        NodeData nd;
-        EdgeData ed;
-        JsonNode jn;
-        JsonEdge je;
-        List<String> classes = new ArrayList<>();
-        classes.add(0,"postype");
-        classes.add(null);
-        for(Node n : g.getNodes()){
-            nd = new NodeData(n.getName(),n.getName());
-            jn = new JsonNode(nd,classes);
-            graph.nodes.add(jn);
-            for(Node n2 : n.getSuccessors().values()){
-                ed = new EdgeData(n.getName()+"_"+n2.getName()+"_edge",n.getName(), n2.getName());
-                je = new JsonEdge(ed,classes);
-                graph.edges.add(je);
-            }
-        }
-        return new ObjectMapper().writeValueAsString(graph);
     }
 }
--- src/test/java/JsonTest.java
+++ src/test/java/JsonTest.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class JsonTest {
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.reactionSystem.Graph;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class JsonTest {
+    private Graph graph;
+
+    @BeforeEach
+    void setUp() {
+        UtilTest.setUp();
+    }
+
+    @Test
+    void JSON_reflexive() throws JsonProcessingException {
+        graph = UtilTest.graph1;
+        assertEquals(Graph.fromJSON(graph.toJson()).toJson(), graph.toJson());
+    }
+
+    @Test
+    void fixedPoint2() throws JsonProcessingException {
+        graph = UtilTest.graph2;
+        assertEquals(Graph.fromJSON(graph.toJson()).toJson(), graph.toJson());
+    }
 }
